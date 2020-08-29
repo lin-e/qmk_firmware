403ac5e5f307a4bd71fa46930cdbac7004126068
me@eugenel.in <me@eugenel.in>
2020-08-29 13:18:30 +0100

    Add ESC tap dance

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/lin-e/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/lin-e/keymap.c
@@ -1,18 +1,45 @@
 #include QMK_KEYBOARD_H
 
+enum {
+    D_BASE = 0,
+    D_ESC
+};
+
 #define _BL 0
 #define _FL 1
 #define _ML 2
 
 #define _______ KC_TRNS
 #define ___X___ KC_NO
+#define TD_ESC TD(D_ESC)
+
+void dance_esc_each(qk_tap_dance_state_t *state, void *user_data) {
+    register_code(KC_ESC);
+}
+void dance_esc_finished(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 3) {
+        register_code(KC_LALT);
+        register_code(KC_Q);
+    }
+}
+void dance_esc_reset(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 3) {
+        unregister_code(KC_LALT);
+        unregister_code(KC_Q);
+    }
+    unregister_code(KC_ESC);
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [D_ESC] = ACTION_TAP_DANCE_FN_ADVANCED(dance_esc_each, dance_esc_finished, dance_esc_reset)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT_all(
-	KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, ___X___, KC_PSCR,
+	TD_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, ___X___, KC_PSCR,
 	KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_NUHS, KC_DEL ,
 	KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_PGUP,
-	KC_LSFT, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , KC_PGDN,
+	KC_LSPO, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSPC, KC_UP  , KC_PGDN,
 	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , MO(_FL), MO(_ML), KC_RALT, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_FL] = LAYOUT_all(
